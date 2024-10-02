e2f562636ff5197806f0bf41d858346c30fe3af4
Adonis Dias <adonisdelima1@gmail.com>
2024-10-01 21:19:45 -0300

    iterating arrays with map method and returning a li for each item inside products array

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,18 @@
+const products = [
+  {name: 'product1', price: 100.00},
+  {name: 'product2', price: 200.00},
+]
+
 function App() {
   return (
     <div>
       <h1 style={{color: 'red'}}>Re-Store</h1>
+      <ul>
+        {/* This is a piece of js code inside the tsx/pseudo html return  */}
+        {products.map(item => (
+          <li>{item.name} - {item.price}</li>
+        ))}
+      </ul>
     </div>
   )
 }
