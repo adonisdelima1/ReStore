457b48691d34ed8aa74cba8419838c3e8ece09fa
Adonis Dias <adonisdelima1@gmail.com>
2024-10-01 21:24:07 -0300

    add a unique key for each li

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,7 @@
 const products = [
   {name: 'product1', price: 100.00},
   {name: 'product2', price: 200.00},
+  {name: 'product2', price: 200.00},
 ]
 
 function App() {
@@ -9,8 +10,8 @@ function App() {
       <h1 style={{color: 'red'}}>Re-Store</h1>
       <ul>
         {/* This is a piece of js code inside the tsx/pseudo html return  */}
-        {products.map(item => (
-          <li>{item.name} - {item.price}</li>
+        {products.map((item, index) => (
+          <li key={index}>{item.name} - {item.price}</li>
         ))}
       </ul>
     </div>
