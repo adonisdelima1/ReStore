a6edd02140f6f09210b2383b42d605b223fb95ec
Adonis Dias <adonisdelima1@gmail.com>
2024-10-07 13:53:54 -0300

    change ProductList to make it use ProductCard

--- client/src/features/catalog/ProductList.tsx
+++ client/src/features/catalog/ProductList.tsx
@@ -1,5 +1,6 @@
-import { Avatar, List, ListItem, ListItemAvatar, ListItemText } from "@mui/material";
+import { List } from "@mui/material";
 import { Product } from "../../app/models/Product";
+import ProductCard from "./ProductCard";
 
 interface Props {
     products: Product[]   
@@ -9,12 +10,7 @@ export default function ProductList({products}: Props) {
     return(
         <List>
             {products.map((product: any) => (
-                <ListItem key={product.id}>
-                    <ListItemAvatar>
-                        <Avatar src={product.pictureUrl} />
-                    </ListItemAvatar>
-                    <ListItemText>{product.name} - {product.price}</ListItemText>
-                </ListItem>
+                <ProductCard product={product} />
             ))}
         </List>
     )
