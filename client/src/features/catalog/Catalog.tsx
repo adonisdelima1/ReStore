a15ff4dfdbf80330d0247a3891b5f223007ba43f
Adonis Dias <adonisdelima1@gmail.com>
2024-10-08 21:31:20 -0300

    move list of products state and api fetch operation into catalog component

--- client/src/features/catalog/Catalog.tsx
+++ client/src/features/catalog/Catalog.tsx
@@ -1,17 +1,19 @@
-import { Avatar, Button, List, ListItem, ListItemAvatar, ListItemText } from "@mui/material"
 import { Product } from "../../app/models/Product"
 import ProductList from "./ProductList"
+import { useEffect, useState } from "react"
 
-interface CatalogProps {
-    products: Product[]
-    addProduct: () => void
-}
+export default function Catalog() {
+    const [products, setProducts] = useState<Product[]>([])
 
-export default function Catalog({products, addProduct}: CatalogProps) {
+    useEffect(() => {
+        fetch('http://localhost:5000/api/products')
+            .then(response => response.json())
+            .then(data => setProducts(data))
+    }, [])
+    
     return (
         <>
             <ProductList products={products}></ProductList>
-            <Button variant="contained" onClick={addProduct}>Add product</Button>
         </>
     )
 }
